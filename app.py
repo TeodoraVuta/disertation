51d6b3596be35d6ab8b81aee891a54a97a261330
TeodoraVuta <vutamaria20@stud.ase.ro>
2025-02-10 18:00:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ ola *mami* que ***guapa*** """)
 tickerSymbol = "COUR"
 
 tickerData = yf.Ticker(tickerSymbol)
-tickerDf=tickerData.history(start='2010-5-31', end='2020-5-31')	
+tickerDf=tickerData.history(start='2010-5-31', end='2025-1-31')	
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
