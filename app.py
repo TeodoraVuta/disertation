6948d0afa57e11927e5dba05342a2a55ba793037
TeodoraVuta <vutamaria20@stud.ase.ro>
2025-02-10 16:52:08 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import yfinance as yf
 
-st.title('Hello World!')
+st.write("""
+# Hello World!
+ola *mami* que ***guapa*** """)
+
+tickerSymbol = "COUR"
+
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf=tickerData.history(start='2010-5-31', end='2020-5-31')	
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
